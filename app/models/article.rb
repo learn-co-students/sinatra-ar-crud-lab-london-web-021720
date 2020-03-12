bb8a792855057bba8482a7054414c60790507d7e
dinnosaur <dinnoroni@gmail.com>
2020-03-12 16:00:16 +0000

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    
+    # attr_accessor :title, :content
+
+    # def initialize(title: title, content: content) 
+    #     @title = title 
+    #     @content = content
+      
+    # end 
+
+
+
+
 end
